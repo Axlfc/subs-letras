93bae8a064cf9cc7bcdf9c4f6190762e4463a7f8
Axel <axelfernandezcurros@gmail.com>
2026-09-17 23:27:16 +0200

    Create subsletra function for subtitle extraction
    Add function to extract subtitles from music videos.

---
+++ subsletra.fish
@@ -0,0 +1,3 @@
+function subsletra --description 'Extrae subtítulos de un vídeo musical separando voz e instrumental (Demucs + whisper.cpp)'
+    python3 $HOME/.config/scripts/subs_letras.py $argv
+end
